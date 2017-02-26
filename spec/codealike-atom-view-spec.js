ee49bbb4160faf05d79e22be368ca76c011c9a26
Daniel Iglesias <diglesias17@hotmail.com>
2017-02-26 20:51:19 -0300

    Initial commit

---
+++ spec/codealike-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodealikeAtomView from '../lib/codealike-atom-view';
+
+describe('CodealikeAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
