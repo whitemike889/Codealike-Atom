e4c8209c773296624e28d396c2ffa45fe056334d
Daniel Iglesias <diglesias17@hotmail.com>
2017-07-16 17:47:43 -0300

    Settings view in progress Authentication process agains server in progress

--- spec/codealike-atom-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CodealikeAtomView from '../lib/codealike-atom-view';
-
-describe('CodealikeAtomView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
---
+++ spec/codealike-settings-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodealikeSettingsView from '../lib/codealike-settings-view';
+
+describe('CodealikeSettingsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
