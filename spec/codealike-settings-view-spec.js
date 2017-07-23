c4cff9d1e5ea2b36f69344de330fac3ba5bbe3a3
Daniel Iglesias <diglesias17@hotmail.com>
2017-07-23 15:06:31 -0300

    Codealike settings custom view removed Codealike configuration added before tracking start

--- spec/codealike-settings-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CodealikeSettingsView from '../lib/codealike-settings-view';
-
-describe('CodealikeSettingsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
